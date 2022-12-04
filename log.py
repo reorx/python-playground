74e86c19ca047258ce5d569fc7d4b1c4b37646fd
Reorx <novoreorx@gmail.com>
2022-12-04 23:02:48 +0800

    add log.py

---
+++ log.py
@@ -0,0 +1,50 @@
+import logging
+
+
+def configure_logger(lg, level, log_format=None, propagate=False):
+    if not log_format:
+        log_format = '%(name)s\t%(levelname)s\t%(asctime)s\t[%(funcName)s] %(message)s'
+    lg.setLevel(level)
+    hdr = logging.StreamHandler()
+    hdr.setFormatter(logging.Formatter(log_format, '%Y-%m-%dT%H:%M:%S'))
+    lg.addHandler(hdr)
+    lg.propagate = propagate
+
+
+def set_logger(name,
+               level='INFO',
+               fmt=None,
+               datefmt=None,
+               propagate=True,
+               remove_handlers=False):
+    """
+    This function will clear the previous handlers and set only one handler,
+    which will only be StreamHandler for the logger.
+
+    This function is designed to be able to called multiple times in a context.
+
+    Note that if a logger has no handlers, it will be added a handler automatically when it is used.
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(getattr(logging, level))
+    logger.propagate = propagate
+
+    if remove_handlers:
+        logger.handlers = []
+        return
+
+    handler = None
+    for h in logger.handlers:
+        if isinstance(h, logging.StreamHandler):
+            # use existing instead of clean and create
+            handler = h
+            break
+    if not handler:
+        handler = logging.StreamHandler()
+        logger.addHandler(handler)
+
+    formatter_kwgs = {}
+    for i in ('fmt', 'datefmt'):
+        if locals()[i] is not None:
+            formatter_kwgs[i] = locals()[i]
+    handler.setFormatter(logging.Formatter(**formatter_kwgs))
