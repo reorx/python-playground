b7bcc25e704cb83ce9999bd62e43792c2c28455e
Reorx <novoreorx@gmail.com>
2023-03-12 13:26:30 +0800

    update urllib_util

--- utils/urllib_util.py
+++ utils/urllib_util.py
@@ -4,7 +4,15 @@ from urllib import request, parse
 from http.client import HTTPResponse, IncompleteRead
 
 
-def http_request(method, url, params=None, headers=None, data: Optional[Union[dict, list, bytes]] = None, timeout=None, logger=None) -> Tuple[HTTPResponse, str]:
+# overload is too complicated
+#@overload
+#def http_request(method, url, params=None, headers=None, data: Optional[Union[dict, list, bytes]] = None, return_bytes: Literal[True] = True, timeout=None, logger=None) -> Tuple[HTTPResponse, bytes]: ...
+#
+#@overload
+#def http_request(method, url, params=None, headers=None, data: Optional[Union[dict, list, bytes]] = None, return_bytes: Literal[False] = False, timeout=None, logger=None) -> Tuple[HTTPResponse, str]: ...
+
+
+def http_request(method, url, params=None, headers=None, data: Optional[Union[dict, list, bytes]] = None, timeout=None, logger=None) -> Tuple[HTTPResponse, bytes]:
     if params:
         url = f'{url}?{parse.urlencode(params)}'
     if not headers:
@@ -16,12 +24,20 @@ def http_request(method, url, params=None, headers=None, data: Optional[Union[di
     if logger:
         logger.info(f'request: {method} {url}')
     req = request.Request(url, method=method, headers=headers, data=data)
-    res = request.urlopen(req, timeout=timeout)  # raises: (HTTPException, URLError)
+    res = request.urlopen(req, timeout=timeout)  # raises: (HTTPException, urllib.error.URLError)
     try:
-        body: str = res.read().decode()
+        body_b: bytes = res.read()
     except IncompleteRead as e:
-        body: str = e.partial.decode()
+        body_b: bytes = e.partial
     if logger:
-        logger.debug(f'response: {res.status}, {body}')
-    return res, body
+        logger.debug(f'response: {res.status}, {body_b}')
+    return res, body_b
 
+
+class RequestError(Exception):
+    def __init__(self, status, body) -> None:
+        self.status = status
+        self.body = body
+
+    def __str__(self):
+        return f'{self.__class__.__name__}: {self.status}, {self.body}'
